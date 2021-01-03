da119ad25e85ffca4cdd738e9deeffe6ff9425db
StellaLulu <cty0220@gmail.com>
2021-01-03 15:11:43 +0800

    Done backend and modification

--- src/App.js
+++ src/App.js
@@ -155,7 +155,10 @@ class App extends Component{
           ? 
           <div>
               <Logo />
-              <Rank name={this.state.user.name} entries={this.state.user.entries}/>
+              <Rank 
+                name={this.state.user.name}
+                entries={this.state.user.entries}
+              />
               <ImageLinkForm 
                 onInputChange={this.onInputChange} 
                 onButtonSubmit={this.onButtonSubmit}/>
